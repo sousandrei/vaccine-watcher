4c83d3674ad071c35b7100ff6437ee04948ca7ef
Andrei Sousa <sousandrei@gmail.com>
2021-07-29 03:10:50 +0200

    ✨ Redis caching temporary values

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/PuerkitoBio/goquery v1.7.0
+	github.com/go-redis/redis/v8 v8.11.0
 	github.com/pkg/errors v0.9.1 // indirect
 	gopkg.in/tucnak/telebot.v2 v2.3.5
 )
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
@@ -9,8 +10,11 @@ import (
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/go-redis/redis/v8"
 	tb "gopkg.in/tucnak/telebot.v2"
 )
+
+const VACCINE_KEY = "vaccine-open"
 
 func createBot() *tb.Bot {
 	botToken := os.Getenv("BOT_TOKEN")
@@ -33,8 +37,13 @@ func handleMe(m *tb.Message) {
 	log.Println(m.Sender.ID, m.Text)
 }
 
+var ctx = context.Background()
+
 func main() {
 	chatId, err := strconv.ParseInt(os.Getenv("CHAT_ID"), 10, 64)
+
+	redisHost := os.Getenv("REDIS_HOST")
+	redisPass := os.Getenv("REDIS_PASSWORD")
 
 	if err != nil {
 		log.Fatalln("CHAT_ID not present")
@@ -46,7 +55,12 @@ func main() {
 	go bot.Start()
 	log.Println("Bot started")
 
-	found := false
+	log.Println("Starting redis client")
+	db := redis.NewClient(&redis.Options{
+		Addr:     redisHost,
+		Password: redisPass,
+		DB:       0,
+	})
 
 	log.Println("Starting loop")
 	for {
@@ -78,15 +92,28 @@ func main() {
 		if len(items) > 0 && strings.Contains(items[0].FirstChild.Data, "stängd") {
 			log.Println("list closed")
 
-			found = false
+			err := db.Set(ctx, VACCINE_KEY, false, 0).Err()
+			if err != nil {
+				bot.Send(&tb.Chat{ID: chatId}, "Failed to set redis key on list close")
+			}
 
 			continue
 		}
 
 		log.Println("list open")
 
+		key, err := db.Get(ctx, VACCINE_KEY).Result()
+		if err != nil {
+			bot.Send(&tb.Chat{ID: chatId}, "Failed to get redis key on list open")
+		}
+
+		found, _ := strconv.ParseBool(key)
+
 		if !found {
-			found = true
+			err := db.Set(ctx, VACCINE_KEY, true, 0).Err()
+			if err != nil {
+				bot.Send(&tb.Chat{ID: chatId}, "Failed to set redis key on list open")
+			}
 
 			log.Println("sending message")
 			bot.Send(&tb.Chat{ID: chatId}, "List open!\nhttps://doktor24.se/vaccin/covid-vaccin/region-stockholm/")
